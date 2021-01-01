286b65a060bcf079edf7ea06118d1d1ad0547bbf
Marjick7 <76257717+Marjick7@users.noreply.github.com>
2021-01-01 09:14:22 +0530

    Add files via upload

---
+++ email_bot.py
@@ -0,0 +1,23 @@
+import smtplib
+import speech_recognition as sr
+
+listener = sr.Recognizer()
+
+try:
+    with sr.Microphone() as source:
+        print('listening...')
+        voice = listener.listen(source)
+        info = listener.recognize_google(voice)
+        print(info)
+except:
+    pass
+
+
+def send_email():
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login('mohamedmarjick123@gmail.com', 'Barakath1234')
+    server.sendmail('mohamedmarjick123@gmail.com',
+                    'mohamedmarjick000@gmail.com',
+                    'hello'
+                    )
